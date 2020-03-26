a210d94c2b9e3d3c7e13c897132288e426c1d047
ldaysjun <ldaysjun@gmail.com>
2020-03-26 21:45:38 +0800

    添加client代码

--- rpc_demo/pkg/api/client.go
+++ rpc_demo/pkg/api/client.go
@@ -1,5 +1,19 @@
 package api
 
-func Client()  {
+import (
+	"github.com/ldaysjun/rpc_learn/protobuf/helloworld"
+	"google.golang.org/grpc"
+	"log"
+)
 
+var address = "localhost:50051"
+
+func NewGreeterClient() helloworld.GreeterClient {
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := helloworld.NewGreeterClient(conn)
+	return c
 }
